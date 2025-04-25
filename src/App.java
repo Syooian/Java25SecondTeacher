a6fd16a905ed4c6207d97e4bf14c3bd8df09245b
Syooian <msn0126@hotmail.com>
2025-04-25 16:29:21 +0800

    備份

--- src/App.java
+++ src/App.java
@@ -1,7 +1,13 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World! I am Edward No. 1!");
 
-        System.out.println("Hello, World! I am Edward No. 17!");
+import java.lang.reflect.Array;
+
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        
+        int []A1={50,66,10,2,69};
+        
     }
 }
