41f760d46f81bca2b2e2bb553227e8628e43680d
Syooian <msn0126@hotmail.com>
2025-04-28 16:01:56 +0800

    備份

--- src/App.java
+++ src/App.java
@@ -5,14 +5,15 @@ public class App {
         // System.out.println("Hello, World!");
 
         // int []A1={50,66,10,2,69};
-        // 99乘法
+        //========================================99乘法
         // for (int a = 2; a <= 9; a++) {
         //     for (int b = 1; b <= 9; b++) {
         //         System.out.print(a + "x" + b + "=" + a * b + "\t");
         //     }
         //     System.err.println("\r");
         // }
-        //金字塔
+        //
+        //========================================金字塔
         /*
          *     *
          *    ***
@@ -30,13 +31,44 @@ public class App {
                 System.out.print(" ");
             }
             //印出*
-            for (int b = 1; b <= 2*a+1; b++) {
+            for (int b = 1; b <= 2 * a + 1; b++) {
                 System.out.print("*");
             }
 
             //Dot += 2;
-
             System.out.println("\r");
         }
+        //
+        //========================================
+        /*從1~100中
+         * 15的倍數輸出"FizzBuzz"
+         * 5的倍數輸出"Buzz"
+         * 3的倍數輸出"Fizz"
+         * 其他數字輸出該數字
+         */
+        for (int a = 1; a <= 100; a++) {
+            if (a % 15 == 0) {
+                System.out.print("FizzBuzz");
+            } else if (a % 5 == 0) {
+                System.out.print("Buzz");
+            } else if (a % 3 == 0) {
+                System.out.print("Fizz");
+            } else {
+                System.out.print(a);
+            }
+
+            System.out.print("\t");
+        }
+
+        //========================================找出最大數
+        int[] Ary = {10, 55, 69, 10, 47, 56, 88, 64, 15};
+        int Max = 0;
+        for (int a = 0; a < Ary.length; a++) {
+            if (Ary[a] > Max) {
+                Max = Ary[a];
+            }
+        }
+        System.out.println("Max : " + Max);
+
     }
 }
